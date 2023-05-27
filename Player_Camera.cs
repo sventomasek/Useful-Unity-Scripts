ea6d78b114f594f5c1990c31c2f6a72e295e8a02
Sven <61661787+sventomasek@users.noreply.github.com>
2023-05-27 19:22:43 +0200

    Updated Player_Camera.cs

--- Player_Camera.cs
+++ Player_Camera.cs
@@ -5,62 +5,57 @@ public class Player_Camera : MonoBehaviour
     public Transform target;
 
     [Header("Settings")]
-    [SerializeField] Vector2 fixedOffset = new Vector2(0, 0);
-    [SerializeField] Vector2 smoothTime = new Vector2(0.05f, 0.05f);
+    [SerializeField] Vector3 fixedOffset = new Vector3(0, 0.5f, 0);
+    [SerializeField] Vector2 smoothness = new Vector2(0.1f, 0.25f);
     [Space]
-    [SerializeField] Vector2 mouseMoveAmount = new Vector2(12, 12);
-    [SerializeField] Vector2 joystickMoveAmount = new Vector2(2, 2);
+    [SerializeField] float mouseMoveAmount = 1;
+    [SerializeField] float joystickMoveAmount = 1;
 
-    private Vector2 mouseOffset;
+    private bool usingMouse = true;
+    private Vector2 mousePosition;
     private Vector2 joystickOffset;
     private Vector2 velocity;
 
-    private Vector2 focusPoint;
-    private bool usingController;
-
     void Update()
     {
-        // Mouse Offset
-        if (mouseMoveAmount != Vector2.zero)
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            focusPoint.x = (target.position.x + mousePos.x) / mouseMoveAmount.x;
-            focusPoint.y = (target.position.y + mousePos.y) / mouseMoveAmount.y;
-        }
+        // Get Mouse Position
+        Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        if (screenRect.Contains(Input.mousePosition)) mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Switch to Mouse Input
-        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1)) usingController = false;
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1)) usingMouse = true;
+
+        // Switch to Controller Input
+        if (joystickOffset != Vector2.zero) usingMouse = false;
 
         // Controller Offset
-        if (joystickMoveAmount != Vector2.zero) joystickOffset = new Vector2(Input.GetAxis("Horizontal Right") * joystickMoveAmount.x, Input.GetAxis("Vertical Right") * joystickMoveAmount.y);
-        
-        // Switch to Controller Input
-        if (joystickOffset != Vector2.zero) usingController = true;
+        if (joystickMoveAmount != 0) 
+            joystickOffset = new Vector2(Input.GetAxis("Horizontal Right") * joystickMoveAmount, Input.GetAxis("Vertical Right") * joystickMoveAmount);
     }
 
     void FixedUpdate()
     {
-		// Camera movement
-        if (target != null)
+        // Multiplying by 0.1363 will give roughly the same amount of Offset to both Mouse and Controller Input
+        Vector2 myPosition = Vector2.Lerp(target.position + fixedOffset, mousePosition, mouseMoveAmount * 0.1363f);
+
+        float posX = 0;
+        float posY = 0;
+
+        // Smooth out the Camera movement
+        if (usingMouse)
         {
-            float posX = 0;
-            float posY = 0;
-
-            // Calculate cameras position based on the targets position
-            if (usingController)
-            {
-                posX = Mathf.SmoothDamp(transform.position.x, target.position.x + fixedOffset.x + joystickOffset.x, ref velocity.x, smoothTime.x);
-                posY = Mathf.SmoothDamp(transform.position.y, target.position.y + fixedOffset.y + joystickOffset.y, ref velocity.y, smoothTime.y);
-            }
-            else
-            {
-                posX = Mathf.SmoothDamp(transform.position.x, target.position.x + fixedOffset.x + focusPoint.x, ref velocity.x, smoothTime.x);
-                posY = Mathf.SmoothDamp(transform.position.y, target.position.y + fixedOffset.y + focusPoint.y, ref velocity.y, smoothTime.y);
-            }
-
-            // Apply cameras position
-            transform.position = new Vector3(posX, posY, transform.position.z);
+            // Mouse Input
+            posX = Mathf.SmoothDamp(transform.position.x, myPosition.x, ref velocity.x, smoothness.x);
+            posY = Mathf.SmoothDamp(transform.position.y, myPosition.y, ref velocity.y, smoothness.y);
         }
+        else
+        {
+            // Controller Input
+            posX = Mathf.SmoothDamp(transform.position.x, target.position.x + fixedOffset.x + joystickOffset.x, ref velocity.x, smoothness.x);
+            posY = Mathf.SmoothDamp(transform.position.y, target.position.y + fixedOffset.y + (joystickOffset.y * 0.55f), ref velocity.y, smoothness.y);
+        }
+
+        // Apply the new position
+        transform.position = new Vector3(posX, posY, transform.position.z);
     }
 }
